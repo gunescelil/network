2659711bed0f0af5b697b1b5c33baa029a59a91c
gunescelil <gunescelil@gmail.com>
2016-11-23 18:40:31 +0300

    Deneme için server tarafına rich text box oydum

---
+++ Server/Server/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serverMonitor = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // serverMonitor
+            // 
+            this.serverMonitor.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.serverMonitor.Location = new System.Drawing.Point(51, 143);
+            this.serverMonitor.Name = "serverMonitor";
+            this.serverMonitor.Size = new System.Drawing.Size(388, 224);
+            this.serverMonitor.TabIndex = 0;
+            this.serverMonitor.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 475);
+            this.Controls.Add(this.serverMonitor);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox serverMonitor;
+    }
+}
+
